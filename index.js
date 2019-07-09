fa91e4fbdcadd7d11d216db5527701a57724ba16
shravan1998 <shrkum51@gmail.com>
2019-07-09 10:33:34 +0530

    Connection is done

---
+++ index.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+var bodyparser = require('body-parser');
+var path = require('path');
+var ejs = require('ejs');
+var session = require('express-session');
+var express = require('express');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    username:'root',
+    password:'',
+    database: 'internship'   
+});
+
+const app  = express();
+
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use(bodyparser.json());
+
+app.use(express.static(__dirname+'/'));
+
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+app.set('views',__dirname);
+
+app.set('view engine','ejs');
+
+app.listen('8000',function(){
+    console.log('connected');
+});
