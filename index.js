387ffce97cd1ad00604460c0389631fee57cd4aa
shravan1998 <shrkum51@gmail.com>
2019-07-09 11:54:36 +0530

    validator added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var path = require('path');
 var ejs = require('ejs');
 var session = require('express-session');
 var express = require('express');
+var validator = require('express-validator');
 
 const connection = mysql.createConnection({
     host:'localhost',
@@ -23,7 +24,13 @@ app.use(express.static(__dirname+'/'));
 app.get('/',function(req,res){
     res.render('index');
 });
-
+app.post('/',function(req,res){
+    var username = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var cpassword = req.body.cpassword;
+}
+);
 app.set('views',__dirname);
 
 app.set('view engine','ejs');
