edc3b40db15a33852f0b0040ec5574a9d31e4e9a
shravan1998 <shrkum51@gmail.com>
2019-07-10 11:57:03 +0530

    Displayed database

--- index.js
+++ index.js
@@ -34,14 +34,41 @@ connection.query('CREATE TABLE IF NOT EXISTS user(NAME CHAR(255) NOT NULL,EMAIL 
         console.log(err);
     }
 );
-app.post('/',function(req,res,next){
-    var username = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
-    var cpassword = req.body.cpassword;
-   //if(password==cpassword &&)
-
-
+app.post('/submit',function(req,res){
+    //var obj={};
+    var name= req.body.name;
+    var email=req.body.email;
+    var password=req.body.password;
+    var cpassword=req.body.cpassword;
+    if(password.length>=6 && cpassword==password){
+        let sql="INSERT INTO user VALUES('"+name+"','"+email+"','"+password+"')";
+        connection.query(sql,function(err,results,field){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(results);
+            }
+           return res.render('/');
+        });
+    }
+    
+});
+app.post('/login',function(req,res){
+    var obj={};
+    var email=req.body.email;
+    var password=req.body.password;
+    let sql="SELECT * FROM `user` WHERE `email`='"+email+"' AND `password`='"+password+"';";
+    connection.query(sql,function(err,results,field){
+        if(err){
+            console.log(err);
+            return res.redirect('/');
+        }
+        else{
+            console.log(results);
+        }
+        return res.render('routes/userdb',{user:results});
+    });
 });
 app.set('views',__dirname);
 
